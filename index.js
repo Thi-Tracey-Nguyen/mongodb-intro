5d178b04b67c24132691ea557f9538848dface8a
Thi-Tracey_Nguyen <traceynguyen.1988@gmail.com>
2022-12-14 21:51:22 +0700

    Lesson completed

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ app.get('/categories', (req, res) => res.send(categories))
 
 app.get('/entries', (req, res) => res.send(entries))
 
+app.get('/entries/:id', (req, res) => {
+    const entry = entries[req.params.id]
+
+    if (entry) {
+        res.status(200).send(entries[req.params.id])
+    } else {
+        res.status(404).send('Entry not found')
+    }
+})
+
 app.post('/entries', (req, res) => {
     // 1. Create a new entry object with values passed in from the request 
     const { category, content } = req.body
