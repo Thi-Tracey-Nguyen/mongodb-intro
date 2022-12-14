f344d56b654b92b28fa8722064bcdbc818047acd
Thi-Tracey_Nguyen <traceynguyen.1988@gmail.com>
2022-12-14 21:43:17 +0700

    create new entry and send res

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 import express from 'express'
 
-const categories = ['Food', 'Book', 'Study', 'Music']
+const categories = ['Food', 'Coding', 'Work', 'Other']
+
+const entries = [
+    {category: 'Food', content: 'Pizza'}, 
+    {category: 'Coding', content: 'Express is cool!'}, 
+    {category: 'Work', content: 'Another day, another dollar'}
+]
 
 const app = express()
 const port = 4001
 
+app.use(express.json())
+
 app.get('/', (request, response) => response.send({ info: 'API' }))
 
-app.get('/categories', (req, res) => res.status(204).send(categories))
+app.get('/categories', (req, res) => res.send(categories))
+
+app.get('/entries', (req, res) => res.send(entries))
+
+app.post('/entries', (req, res) => {
+    // 1. Create a new entry object with values passed in from the request 
+    const { category, content } = req.body
+    // Sanitize the request 
+    const newEntry = {category, content}
+
+    // 2. Push the new entry to the entries array
+    entries.push(newEntry)
+    // 3. Send the new entry with 201 status
+    res.status(201).send(newEntry)
+})
 
 app.listen(port, () => console.log(`App is running at http://localhost:${port}`))
