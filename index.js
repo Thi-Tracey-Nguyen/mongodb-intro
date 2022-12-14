277829d4cc4de510112a7ca910e879c5faaed34b
Thi-Tracey_Nguyen <traceynguyen.1988@gmail.com>
2022-12-14 08:32:52 +0700

    added categories route

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from 'express'
 
+const categories = ['Food', 'Book', 'Study', 'Music']
+
 const app = express()
 const port = 4001
 
+app.get('/', (request, response) => response.send({ info: 'API' }))
 
+app.get('/categories', (req, res) => res.status(204).send(categories))
 
 app.listen(port, () => console.log(`App is running at http://localhost:${port}`))
